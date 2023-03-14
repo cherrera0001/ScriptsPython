b75e21ba3f9c7a4f2f780329bcf5e2caf17955b0
cherrera0001 <herrera.jara.cristobal@gmail.com>
2023-03-14 19:16:54 -0300

    Update scanNet.py
    Se actualiza la forma en que recorre, ahora tomará 1 ip probará todos los puertos el rango declarado y saltará a la siguiente ip.

--- scanNet.py
+++ scanNet.py
@@ -1,33 +1,24 @@
 import socket
 from multiprocessing import Pool
+from tqdm import tqdm
 
 # Define el rango de direcciones IP a escanear
-ip_base = "172.16.1."
+ip_base = "192.168.1."
 # Define el rango de puertos a escanear
 port_range = range(1, 1025)
 
 def scan_port(port):
-    # Crea una lista para almacenar los resultados
     results = []
-    
-    # Itera sobre las direcciones IP y los puertos a escanear
+
     for ip in range(1, 255):
-        # Crea un objeto socket
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-            # Establece el tiempo de espera de conexión en 1 segundo
             client.settimeout(1)
-            
-            # Intenta conectarse al puerto especificado en la dirección IP actual
+
             try:
                 client.connect((ip_base + str(ip), port))
-                
-                # Envía un mensaje de solicitud al servicio
                 client.sendall(b"GET / HTTP/1.1\r\nHost: example.com\r\n\r\n")
-                
-                # Recibe la respuesta del servicio
                 response = client.recv(4096)
 
-                # Analiza la respuesta para determinar el servicio que está en ejecución
                 if b"HTTP" in response:
                     service = "HTTP"
                 elif b"SSH" in response:
@@ -36,8 +27,7 @@ def scan_port(port):
                     service = "SMTP"
                 else:
                     service = "Unknown"
-                
-                # Guarda el resultado en la lista de resultados
+
                 result = {
                     "ip": ip_base + str(ip),
                     "port": port,
@@ -45,20 +35,16 @@ def scan_port(port):
                 }
                 results.append(result)
 
+                print(f"IP: {result['ip']}, Port: {result['port']}, Service: {result['service']}")
+
             except:
                 pass
 
     return results
 
 if __name__ == '__main__':
-    # Crea un grupo de procesos para escanear los puertos
+    results = []
+
     with Pool(processes=4) as pool:
-        # Escanea los puertos en paralelo utilizando los procesos del grupo
-        results = pool.map(scan_port, port_range)
-
-    # Concatena los resultados de los escaneos de cada puerto
-    results = [item for sublist in results for item in sublist]
-
-    # Imprime los resultados una vez que se completa el escaneo
-    for result in results:
-        print(result)
+        for result in tqdm(pool.imap_unordered(scan_port, port_range), total=len(port_range)):
+            results.extend(result)
