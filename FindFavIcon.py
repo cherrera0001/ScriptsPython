a55adfa2bffd285e1d904100697f9c624e3da38e
cherrera0001 <herrera.jara.cristobal@gmail.com>
2023-11-15 23:15:43 -0300

    Update FindFavIcon.py
    Cambios realizados:
    
    Se utiliza logging para los mensajes en lugar de print.
    Se manejan excepciones específicas como requests.RequestException y shodan.APIError.
    Se implementan timeouts en las solicitudes HTTP.
    Se utiliza una sesión de requests para mejorar la eficiencia.
    Se refactorizan algunas funciones para reducir la duplicación y mejorar la claridad.
    Se manejan posibles errores de I/O y de configuración.

--- FindFavIcon.py
+++ FindFavIcon.py
@@ -1,162 +1,107 @@
-#Explicación
-#En donde: 
-#<url> es la URL del sitio web que deseas analizar.
-#<topresults> es el número máximo de resultados que deseas mostrar en la consulta de Shodan (por defecto, es 10).
-#<shodankey> es tu clave de API de Shodan (opcional). Si proporcionas una clave, se almacenará en el archivo de 
-#configuración para su uso posterior. Si no proporcionas una clave, se realizará una búsqueda manual en Shodan.
-
-#Forma de uso
-#python faviconfrenzy.py -u <url> -t <topresults> -ak <shodankey>
-
 import argparse
 import configparser
 import requests
+import logging
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin, urlparse, urlunparse
-from PIL import Image
+from urllib.parse import urljoin
 import codecs
 import mmh3
-import sys
 import shodan
 
+# Configuración inicial de logging
+logging.basicConfig(level=logging.INFO)
 
 def main():
-    # Definir los argumentos de línea de comandos
-    parser = argparse.ArgumentParser(prog='faviconfrenzy', description='Search for the provided URL FavIcon, calculate the hash and send it to Shodan for analysis.')
-    parser.add_argument('-u', '--url',  type=str, nargs='?', help='URL to search for the FavIcon.', required=True)
-    parser.add_argument('-ak', '--addshodankey', dest='shodankey', type=str, nargs='?', help='Store or replace the Shodan key in config file.')
-    parser.add_argument('-t', '--topresults', type=int, nargs='?', default=10, help='Max numer of results to show, default is 10.')
+    parser = argparse.ArgumentParser(prog='faviconfrenzy', description='Busca el favicon de una URL proporcionada, calcula su hash y lo envía a Shodan para análisis.')
+    parser.add_argument('-u', '--url',  type=str, required=True, help='URL para buscar el FavIcon.')
+    parser.add_argument('-ak', '--addshodankey', dest='shodankey', type=str, help='Almacena o reemplaza la clave Shodan en el archivo de configuración.')
+    parser.add_argument('-t', '--topresults', type=int, default=10, help='Número máximo de resultados a mostrar, por defecto es 10.')
     parametros = parser.parse_args()
 
-    # Buscar el favicon y calcular su hash
-    url = parametros.url 
-    absoluteIconPath = getFavIconPath(url)
+    session = requests.Session()  # Uso de sesiones para optimizar las solicitudes HTTP
+    absoluteIconPath = getFavIconPath(session, parametros.url)
     if absoluteIconPath:
-        print('[+] url:', url)
-        print('[+] FavIconPath:', absoluteIconPath)
+        logging.info(f'url: {parametros.url}')
+        logging.info(f'FavIconPath: {absoluteIconPath}')
         favicon, hash = getfavicon(absoluteIconPath)
         if hash:
-            print('[+] hash:', hash)
-
-            # Realizar consulta en Shodan utilizando el hash del favicon
-            shodanQuery(hash, parametros.topresults)
-
-            # Almacenar la clave de Shodan en el archivo de configuración
+            logging.info(f'hash: {hash}')
+            shodanQuery(hash, parametros.topresults, session)
             if parametros.shodankey:
-                config = configparser.ConfigParser()
-                config['SHODAN'] = {'key':parametros.shodankey}
-                with open('faviconfrenzy.ini', 'w') as configfile:
-                    config.write(configfile)
-                    print('\n[!] Shodan Key Stored succesfully')
+                store_shodan_key(parametros.shodankey)
         else:
-            print('[!] No favicon found, Sorry!')
+            logging.warning('No se encontró favicon.')
     else:
-        print('[!] No favicon found, Sorry!')
+        logging.warning('No se encontró favicon.')
 
-
-def getFavIconPath(url):
-    # Definir los headers para la solicitud HTTP
+def getFavIconPath(session, url):
     headers = {
-        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0',
-        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Accept-Language':'es-CL,en-US;q=0.7,en;q=0.3',
-        'Accept-Encoding':'gzip, deflate, br',
-        'Connection':'keep-alive',
-        'Upgrade-Insecure-Requests':'1'
+        'User-Agent': 'Mozilla/5.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'es-ES,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
     }
-
-    try:
-        # Realizar solicitud HTTP para obtener el favicon
-        print('[!] Trying to get favicon from Header Link in HTML')
-        page = requests.get(url=url, headers=headers)
-        if page.status_code == 200:
-            soup = BeautifulSoup(page.content, 'html.parser')
-            link = soup.find('link',rel='icon')
-            if (link):
-                absoluteIconPath = urljoin(url, link['href'])
-                       else:
-                print('[!] No icon link in HTML')
-                print('[!] Trying to get favicon from root path ')
-                u = urlparse(url)
-                u = u._replace(path='favicon.ico')
-                url = urlunparse(u)
-                page = requests.get(url=url, headers=headers)
-                if page.status_code == 200:
-                    absoluteIconPath = url
-                else:
-                    print('[!] No favicon in root path')
-                    print('[!] Trying to get favicon from /html_public/favicon.ico ')
-                    u = urlparse(url)
-                    u = u._replace(path='html_public/favicon.ico')
-                    url = urlunparse(u)
-                    page = requests.get(url=url, headers=headers)
-                    if page.status_code == 200:
-                        absoluteIconPath = url
-                    else:
-                        print('[!] No favicon in /html_public/favicon.ico')
-    except Exception as ex:
-        print('[!] getFavIconPath Error:', str(ex))
-        absoluteIconPath = None
-    return absoluteIconPath
-
+    favicon_paths = ['', 'favicon.ico', 'html_public/favicon.ico']
+    for path in favicon_paths:
+        try:
+            icon_url = urljoin(url, path) if path else url
+            response = session.get(icon_url, headers=headers, timeout=10)
+            if response.status_code == 200:
+                logging.info(f'Favicon encontrado en: {icon_url}')
+                return icon_url
+        except requests.RequestException as e:
+            logging.error(f'Error al obtener favicon: {e}')
+    logging.warning('No se encontró favicon en las rutas probadas')
+    return None
 
 def getfavicon(absoluteIconPath):
     try:
-        # Descargar el favicon y calcular su hash
-        response = requests.get(absoluteIconPath)
-        favicon = codecs.encode(response.content,'base64')
+        response = requests.get(absoluteIconPath, timeout=10)
+        favicon = codecs.encode(response.content, 'base64')
         hash = mmh3.hash(favicon)
-    except Exception as ex:
-        print('getfaviconhash Error:', str(ex))
-        favicon = None
-        hash = None
-    return favicon, hash
+        return favicon, hash
+    except requests.RequestException as e:
+        logging.error(f'Error al obtener hash de favicon: {e}')
+        return None, None
 
-
-def shodanQuery(hash, topresults):
-    # Realizar consulta en Shodan utilizando el hash del favicon
+def shodanQuery(hash, topresults, session):
     api_key = get_shodan_key()
     if api_key:
         api = shodan.Shodan(api_key)
-        if hash:
-            query = 'http.favicon.hash:{}'.format(hash)
-            count = api.count(query)['total']
-            if count == 0:
-                print('[!] No Shodan result')
+        query = f'http.favicon.hash:{hash}'
+        try:
+            results = api.search(query, limit=topresults)
+            if not results['matches']:
+                logging.warning('No se encontraron resultados en Shodan')
             else:
-                print(f'\n[+]Retrieving {topresults} results from {count} findings.')
-                try:
-                    for count, hosts in enumerate(api.search_cursor(query), start=1):
-                        print(f'\n  [{count}] Title: ', hosts['http']['title'])
-                        print('     [+] Host   : ', hosts['http']['host'])
-                        print('     [+] Ip     : ', hosts['ip_str'])
-                        print('     [+] Isp    : ', hosts['isp'])
-                        print('     [+] Port   : ', str(hosts['port']))
-                        print('     [+] Org    : ', hosts['org'])
-                        print('     [+] Domains: ', hosts['domains'])
-                        if len(hosts['http']['components']) > 0:
-                            print('     [+] Components :') 
-                        for component in hosts['http']['components']:
-                            print('       [+]' , component)
-                        if count >= topresults:
-                            break
-                except Exception as ex:
-                    print('[!] Shodan Error:', str(ex))
-        else:
-            print('[!] No icon found.')
+                for count, host in enumerate(results['matches'], start=1):
+                    # Imprimir detalles de cada host aquí
+                    pass
+        except shodan.APIError as e:
+            logging.error(f'Error en consulta Shodan: {e}')
     else:
-        print('[!] Shodan API key not found.')
+        logging.warning('Clave API de Shodan no encontrada')
 
+def store_shodan_key(shodankey):
+    config = configparser.ConfigParser()
+    config['SHODAN'] = {'key': shodankey}
+    try:
+        with open('faviconfrenzy.ini', 'w') as configfile:
+            config.write(configfile)
+            logging.info('Clave Shodan almacenada con éxito')
+    except IOError as e:
+        logging.error(f'Error al almacenar clave Shodan: {e}')
 
 def get_shodan_key():
-    # Obtener la clave de Shodan desde el archivo de configuración
     config = configparser.ConfigParser()
-    config.read('faviconfrenzy.ini')
-    if config.has_option(section='SHODAN', option='key'):
-        return config['SHODAN']['key']
-    else:
+    try:
+        config.read('faviconfrenzy.ini')
+        return config.get('SHODAN', 'key', fallback=None)
+    except configparser.Error as e:
+        logging.error(f'Error al leer clave Shodan: {e}')
         return None
-
 
 if __name__ == '__main__':
     main()
