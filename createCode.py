665f7492d4d5eef19caef6b29cd8dbfa26151f5e
cherrera0001 <herrera.jara.cristobal@gmail.com>
2024-07-15 15:21:49 -0400

    Delete createCode.py

--- createCode.py
+++
@@ -1,8 +0,0 @@
-import random
-#Generar códigos nros. largo 6 dígitos.
-codigos = []
-with open("codigos.txt", "w") as f:
-    for i in range(1000):
-        codigo_aleatorio = str(random.randint(0, 999999)).zfill(6)
-        codigos.append(codigo_aleatorio)
-        f.write(codigo_aleatorio + "\n")
